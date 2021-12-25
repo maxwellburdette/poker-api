1cf2bf74cc35230a2fa0e61138ae430629d53dd7
maxwellburdette <77358759+maxwellburdette@users.noreply.github.com>
2021-12-25 01:21:37 -0600

    Adds multi-card drawing
    Removes:
    - Removes BQ operations
    - Removes delete deck
    - Removes unused functions from post.ts
    - Removes deck schema (No longer using BQ)
    
    Updates:
    - Updates deleteCard to support multicard drawing
    - Updates getCard to support multi-card drawing
    - Updates draw function for multi-card drawing

--- src/DB/DeleteCard.ts
+++ src/DB/DeleteCard.ts
@@ -1,23 +1,26 @@
 import db from "../DB/config";
+import Card from "../Classes/Card";
 
 export default async function deleteCard(
 	tableId: string,
-	cardId: Promise<any>
+	cardPromise: Promise<Card[]> | Card[]
 ) {
-	const card = await cardId;
-	if (card) {
-		const sql: string = `DELETE FROM deck.${tableId} WHERE uuid = \"${card.uuid}\";`;
-		return new Promise((resolve, reject) => {
-			db.query(sql, function (error: any, results: any, fields: any) {
-				if (error) {
-					reject(error);
-				} else {
-					resolve(results);
-				}
-			});
-		});
+	const cards: Card[] = await cardPromise;
+	if (cards.length > 0) {
+		let sql: string = `
+			DELETE FROM deck.${tableId} 
+			WHERE uuid IN(${cards.map((card) => {
+				return `\"${card.uuid}\"`;
+			})}) `;
+
+		return runQuery(sql);
 	}
 	const sql: string = `DROP TABLE deck.${tableId};`;
+	return runQuery(sql);
+}
+
+function runQuery(sql: string): Promise<Card> {
+	console.log(sql);
 	return new Promise((resolve, reject) => {
 		db.query(sql, function (error: any, results: any, fields: any) {
 			if (error) {
