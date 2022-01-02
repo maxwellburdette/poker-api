ac55f40289e76249b4c899e784679d898b6c2fc4
maxwellburdette <77358759+maxwellburdette@users.noreply.github.com>
2022-01-02 00:52:06 -0600

    Adds middleware auth

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response, Express } from "express";
+import express, { Request, Response, Express, response } from "express";
+import axios, { AxiosError, AxiosRequestConfig } from "axios";
 import cors from "cors";
 import path from "path";
 import post from "./Routes/post";
@@ -11,10 +12,31 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.use((req: Request, res: Response, next: () => void) => {
-	console.log(Date.now());
-	console.log("middleware");
-	next();
+app.use(async (req: Request, res: Response, next: () => void) => {
+	const accessToken: string | null = req.headers.token
+		? (req.headers.token as string)
+		: null;
+
+	if (!accessToken) {
+		res.status(500).send("Must supply token in headers");
+		return;
+	}
+	var config: AxiosRequestConfig = {
+		method: "get",
+		url: process.env.AUTH_URL,
+		headers: {
+			id_token: accessToken,
+		},
+	};
+	try {
+		const response = await axios(config);
+		console.log(`${response.data.email} authenticated`);
+		next();
+	} catch (e: any) {
+		console.log(e.response.data);
+		res.status(500).send("Authentication Failed");
+		return;
+	}
 });
 app.use("/post", post);
 app.use("/get", get);
